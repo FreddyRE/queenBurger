290e166a139652347ab91fa7bce1b05ebe4302f5
Freddy Romero} <freddy@virtuelle.mx>
2019-11-01 15:13:30 -0600

    agregando componente Menu

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import LoginForm from './LoginForm'
-
+import MainMenu from './MainMenu'
 
 const tagForUserNameField = "1_1"
 
 class App extends React.Component {
 
     render() {
-        return ( <
-            LoginForm tagForUserName = {tagForUserNameField}/ >
+        return ( 
+            <div>
+                <LoginForm tagForUserName = {tagForUserNameField}/>
+                <MainMenu />
+            </div>
+            
         )
     }
 
---
+++ src/Components/MainMenu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+class MainMenu extends React.Component{
+    
+    render() {
+        return(
+            <div>main Menu!!!</div>
+        )
+    }
+}
+
+export default MainMenu
